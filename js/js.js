07ee9b9a18b8efeea32c8529c4aceff2c43cc2cb
Vitor da Fonseca Rodrigues <48792701+VitorFonseca123@users.noreply.github.com>
2022-06-02 18:06:03 -0300

    pontuação
    Foi adicionado pontuação e o sonic vira bolinha quando pula, porém com alguns bugs

--- js/js.js
+++ js/js.js
@@ -1,33 +1,47 @@
 const sonic = document.querySelector('.sonic');
 const spike = document.querySelector('.spike');
-
+let pontos = document.getElementById('pontos');
+var spikePosition = spike.offsetLeft;
+var sonicPosition = +window.getComputedStyle(sonic).bottom.replace('px', '');
+var pont=0;
 function jump() {
+     
     sonic.classList.add('jump');
+    sonic.src = "./img/dash.gif";
+    //sonic.style.width="50px";
     setTimeout(() => {
         sonic.classList.remove('jump');
+        sonic.src = "./img/sonic.gif";
+        //sonic.style.width="50px";
     }, 500);
 }
 
 const loop = setInterval(() => {
-    const spikePosition = spike.offsetLeft;
-    const sonicPosition = +window.getComputedStyle(sonic).bottom.replace('px', '');
+    
+    pontos.innerHTML = pont;
+    
     //const sonicAltura = offsetBottom;
-    console.log(sonicPosition);
-    if (spikePosition <= 80 && sonicPosition < 160 && spikePosition > 0) {
+     spikePosition = spike.offsetLeft;
+     sonicPosition = +window.getComputedStyle(sonic).bottom.replace('px', '');
+    if (spikePosition <= 95 && sonicPosition < 170 && spikePosition > 0) {
         spike.style.animation = 'none';
         spike.style.left = `${spikePosition}px`;
+        
         sonic.style.bottom = `${sonicPosition}px`;
         sonic.src = "./img/sonicDeath.png";
         setTimeout(() => {
             sonic.style.animation = 'deathSonic 500ms';
             sonic.style.bottom = '-100px';
         }, 500);
+      
        
         
         
         
-        
         clearInterval(loop);
+    }else if(spikePosition<=95 && spikePosition<-70){
+        pont=pont+100;
+        console.log(spikePosition);
     }
 }, 10);
 document.addEventListener('keydown', jump);
